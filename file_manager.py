cf7a45553ad9c35a356e53df9b39225b72bb8dbd
AlexRos <63934839+AlexRos10@users.noreply.github.com>
2020-08-30 22:52:49 +0200

    Add files via upload

---
+++ file_manager.py
@@ -0,0 +1,38 @@
+import os
+
+class File_Manager(object):
+    def __init__(self, work_area):
+        os.chdir(work_area)
+
+    def rename_files(self):
+        with os.scandir(work_area) as files:
+            for file in files:
+                source = work_area + '\\' + file.name
+                file_name = file.name
+                try:
+                    new_name = input('Rename: {} \n >>> '.format(file_name))
+                    if new_name == 'exit':
+                        break
+                    if not new_name == 'pass':
+                        desti = work_area + '\\' + new_name
+                        os.rename(source, desti)
+                except Exception as e:
+                    print('Invalid Filename')
+                    new_name = input('Rename: {} \n >>> '.format(file_name))
+                    if new_name == 'exit':
+                        break
+                    if not new_name == 'pass':
+                        desti = work_area + '\\' + new_name
+                        os.rename(source, desti)
+
+def selectPath():
+    path = input('Enter the path C:\\Users\\Rosar\\ \n >>> ')
+    if path == '':
+        path = 'Desktop'
+
+    return path
+
+if __name__ == '__main__':
+    work_area = os.path.join(os.path.expanduser('~'), selectPath())
+    file = File_Manager(work_area)
+    file.rename_files()
---
+++ folder_manager.py
@@ -0,0 +1,43 @@
+import os
+
+class Folder_Management(object):
+    def __init__(self, work_area):
+        try:
+            os.chdir(work_area)
+        except Exception as e:
+            work_area = os.path.join(os.path.expanduser('~'), 'OneDrive\\Desktop')
+            os.chdir(work_area)
+    
+    def create_folders(self, amount = 1):
+        amount = amount
+        while True:
+            try:
+                if amount == 1:
+                    folder_name = input('Enter the Folder Name\n >>> ')
+                    os.mkdir(folder_name)
+                    break
+                else:
+                    for i in range(amount):
+                        folder_name = input('Enter the Folder Name\n >>> ')
+                        os.makedirs(folder_name)
+                        amount -= 1
+                    break
+            except Exception as e:
+                print('Invalid Filname')
+                continue
+
+def quantify_folders():
+    quant_folders = int(input('How many folders do you wanna create\n >>> '))
+    return quant_folders
+
+def selectPath():
+    path = input('Enter the path C:\\Users\\Rosar\\ \n >>> ')
+    if path == '':
+        path = 'Desktop'
+
+    return path
+    
+if __name__ == '__main__':
+    work_area = os.path.join(os.path.expanduser('~'), selectPath())
+    folder = Folder_Management(work_area)
+    folder.create_folders(quantify_folders())
