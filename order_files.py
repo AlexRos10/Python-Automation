2f4ca4492580798120b395a8e144ca020afbb4e9
Alex Ros <63934839+AlexRos10@users.noreply.github.com>
2021-01-13 09:40:05 +0100

    Update order_files.py

--- order_files.py
+++ order_files.py
@@ -11,14 +11,16 @@ def order_files(work_area):
     with os.scandir(work_area) as files:
         for file in files:
             if os.path.isfile(file):
+                os.chdir(work_area)
                 extens = file.name.find('.')
                 extension = file.name[extens::]
                 if not file.name.lower().find('wallpaper') == -1:
-                    file_name = file.name[::extens] + '.png'
+                    file_name = file.name[0:extens] + '.png'
+                    print(file.name, file.name[0:extens], file_name)
                     path = 'Pictures\\Imagenes\\Wallpapers\\' + file_name
-                elif extension in image_extensions[::-1]:
+                elif extension in image_extensions[0:-1]:
                     if not extension == '.png':
-                        file_name = file.name[::extens] + '.png'
+                        file_name = file.name[0:extens] + '.png'
                     else:
                         file_name = file.name
                     path = 'Pictures\\Imagenes\\' + file_name
@@ -33,6 +35,24 @@ def order_files(work_area):
                         path = 'Pictures\\Audios\\' + file.name                   
 
                 os.rename(file, os.path.join(os.path.expanduser('~'), path))
+            elif os.path.isdir(file) and file.name != 'MyFlow':
+                os.chdir(file)
+                with os.scandir(file) as folder:
+                    for archive in folder:
+                        if os.path.isfile(archive):
+                            extens = archive.name.find('.')
+                            extension = archive.name[extens::]
+                            if extension in image_extensions[0:-1]:
+                                if not extension == '.png':
+                                    file_name = archive.name[0:extens] + '.png'
+                                else:
+                                    file_name = archive.name
+                                
+                                os.rename(archive, file_name)
+                            
+                    os.chdir('C:\\Users\\Alex Ros\\downloads')
+                    os.rename(file, 'C:\\Users\\Alex Ros\\Pictures\\Imagenes\\' + file.name)
+
 
 if __name__ == '__main__':
     work_area = os.path.join(os.path.expanduser('~'), 'downloads')
