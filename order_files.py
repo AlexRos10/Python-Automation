f7b9cdc17456f2481c5390b6c4a811f25930c916
Alex Ros <63934839+AlexRos10@users.noreply.github.com>
2021-01-14 15:47:14 +0100

    Update order_files.py

--- order_files.py
+++ order_files.py
@@ -1,10 +1,17 @@
-#Maybe you have to change some things because this script is made for my own Downloads Folder
-
+#A Simple Python Script to Order your Downloads Folder (Maybe It Could Update in the Future with new Features)
 import os 
 
 image_extensions = ['.png', '.jpg', '.jpeg', '.gif']
 video_extensions = ['.mov', '.mp4', '.m4v']
 audio_extensions = ['.mp3', '.m4a', '.wav', '.ogg']
+doc_extensions = ['.txt', '.pdf']
+
+docs_folder = '' #Write here the path of your documents folder
+images_folder = '' #Write here the path of your images folder
+wallpapers_folder = '' #Write here the path of your wallpapers folder
+gifs_folder = '' #Write here the path of your gifs folder
+videos_folder = '' #Write here the path of your videos folder
+audios_folder = '' #Write here the path of your audios folder
 
 def order_files(work_area):
     os.chdir(work_area)
@@ -14,28 +21,26 @@ def order_files(work_area):
                 os.chdir(work_area)
                 extens = file.name.find('.')
                 extension = file.name[extens::]
-                if not file.name.lower().find('wallpaper') == -1:
+                if extension in doc_extensions:
+                    path = f'{docs_folder}\\' + file.name
+                elif not file.name.lower().find('wallpaper') == -1:
                     file_name = file.name[0:extens] + '.png'
-                    print(file.name, file.name[0:extens], file_name)
-                    path = 'Pictures\\Imagenes\\Wallpapers\\' + file_name
+                    path = f'{wallpaper_folder}\\' + file_name
                 elif extension in image_extensions[0:-1]:
                     if not extension == '.png':
                         file_name = file.name[0:extens] + '.png'
                     else:
                         file_name = file.name
-                    path = 'Pictures\\Imagenes\\' + file_name
+                    path = f'{images_folder}\\' + file_name
                 elif extension == '.gif':
-                    path = 'Pictures\\Imagenes\\Gifs\\' + file.name
+                    path = f'{gifs_folder}\\' + file.name
                 elif extension in video_extensions:
-                    path = 'Pictures\\Videos\\' + file.name
+                    path = f'{videos_folder}\\' + file.name
                 elif extension in audio_extensions:
-                    if not file.name.find('Python Course Leasson') == -1:
-                        path = 'Desktop\\Python Course\\Audios\\' + file.name
-                    else:
-                        path = 'Pictures\\Audios\\' + file.name                   
+                    path = f'{videos_folder}\\' + file.name                   
 
                 os.rename(file, os.path.join(os.path.expanduser('~'), path))
-            elif os.path.isdir(file) and file.name != 'MyFlow':
+            elif os.path.isdir(file):
                 os.chdir(file)
                 with os.scandir(file) as folder:
                     for archive in folder:
@@ -51,11 +56,9 @@ def order_files(work_area):
                                 os.rename(archive, file_name)
                             
                     os.chdir('C:\\Users\\Alex Ros\\downloads')
-                    os.rename(file, 'C:\\Users\\Alex Ros\\Pictures\\Imagenes\\' + file.name)
+                    os.rename(file, f'{images_folder}\\' + file.name)
 
 
 if __name__ == '__main__':
     work_area = os.path.join(os.path.expanduser('~'), 'downloads')
-    order_files(work_area)
-    work_area = os.path.join(os.path.expanduser('~'), 'downloads\\MyFlow')
     order_files(work_area)
